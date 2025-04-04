d20d48d7208d2b1ebced6ecd68d803430db3f9b0
nikhmatul <hinikhmatulhidayah@gmail.com>
2025-04-04 17:12:15 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\web_controller;
 
-Route::get('/todo', function () {
+Route::get('/', function () {
     return view('todo-all');
 });
 
